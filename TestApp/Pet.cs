7c4e4918615fee28eb7c8c5b0ca1d14057d6a416
Oleksandra Hlushchevska <o.hlushchevska@eleks.com>
2024-01-28 23:51:15 +0200

    Add project files.

---
+++ TestApp/Pet.cs
@@ -0,0 +1,48 @@
+﻿namespace TestApp
+{
+    public class Pet
+    {
+        private string name;
+        private DateTime dateOfBirth;
+        private string sex;
+        private string breed;
+        private double weight;
+
+        public Pet() { }
+
+        public Pet(string name, DateTime dateOfBirth, string sex, string breed, double weight)
+        {
+            this.name = name;
+            this.dateOfBirth = dateOfBirth;
+            this.sex = sex;
+            this.breed = breed;
+            this.weight = weight;
+        }
+
+       public int GetAge() {
+
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - dateOfBirth.Year;
+            if (dateOfBirth.Date > currentDate.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+
+        }
+
+        public string GetName()
+        {
+
+            return name;
+        }
+
+
+        public string GetInfo() {
+
+            return ToString();
+
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using TestApp;
+
+Pet cat1 = new Pet("Mara", new DateTime(2016, 8, 25), "Female", "British", 5.54);
+
+Console.WriteLine($"Cat {cat1.GetName()} is {cat1.GetAge()} years old");
+Console.WriteLine(cat1.GetInfo());
